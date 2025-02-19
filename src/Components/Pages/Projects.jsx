1d1984ca682fa7862f8d0e296c9c3fa04fb5e6f1
battlesofarmy <battlesofarmy@gmail.com>
2025-02-19 18:00:11 +0600

    optimized code

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Navbar from "../Global/Navbar";
 import Footer from "../Global/Footer";
 import SingleProjectCart from "../Global/SingleProjectCart";
@@ -41,23 +40,11 @@ export default function Projects() {
       <section className="pt-20">
         <div className="container">
           <div className="flex gap-5">
-          {/* {
-            items.map(ele=>
-              <Link key={ele} to={`/projects/${ele}`}>
-                <p className="border shadow-lg px-4 py-2 rounded-md">{ele}</p>
-              </Link>
-            )
-          } */}
-
-          <div className="flex gap-6">
             {
               items?.map(ele=>
                 <p key={ele.category} onClick={()=>handleItems(ele.category)} className="border shadow bg-gray-600 text-white px-4 py-2 rounded-md  cursor-pointer">{ele.name}</p>
               )
             }
-          </div>
-
-
           </div>
         </div>
       </section>
