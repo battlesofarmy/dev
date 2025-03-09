113458352e6006a21e5a4063a8adcf531614271a
battlesofarmy <battlesofarmy@gmail.com>
2025-03-09 23:42:52 +0600

    all the skill are added

--- src/pages/details/details-pages/Skills.jsx
+++ src/pages/details/details-pages/Skills.jsx
@@ -7,21 +7,41 @@ import { FaGithub } from "react-icons/fa6";
 import { RiVercelFill, RiNextjsFill } from "react-icons/ri";
 import { SiShadcnui } from "react-icons/si";
 import DetailsSubTitle from "../../../Components/DetailsSubTitle";
+// Backend Techonologys
+import { SiNestjs } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiPrisma } from "react-icons/si";
+// Database
+import { SiPostgresql } from "react-icons/si";
+// auth
+import { RiFirebaseFill } from "react-icons/ri";
+import { RiSupabaseLine } from "react-icons/ri";
+import { SiJsonwebtokens } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+// API
+import { TbApiOff } from "react-icons/tb";
+import { GrGraphQl } from "react-icons/gr";
 
 
 export default function Skills() {
   // Techonology Name 
   const languages = ['TypeScript', 'Javascript', 'C', 'C++'];
-  const backend = ['NodeJs', 'MongoDB', 'Mongoose'];
+  const backend = ['NodeJs','ExpressJs','NestJs','Prisma', 'Mongoose'];
+  const database = ['PostgreSQL','MongoDB'];
   const frontend = [ 'NextJs', 'ReactJs', 'TailwindCSS', "Shadcn Ui"];
   const stateManagement = [ 'Zustand', 'Redux', 'ContextAPI'];
+  const auth = [ 'Firebase', 'Supabase', 'JWT', 'NextJs Auth'];
+  const api = [ 'REST API', 'GraphQL'];
   const tools = ['Git', 'Github', 'Postman', 'Vercel', 'Netlify']
 
   // Icons name List 
   const languagesIcons = ['SiTypescript', 'IoLogoJavascript' ,'FaCopyright', 'CgCPlusPlus'];
-  const backendIcons = ['SiNodedotjs', 'SiMongodb', 'SiMongoose'];
+  const backendIcons = ['SiNodedotjs','SiExpress', 'SiNestjs','SiPrisma', 'SiMongoose'];
+  const databaseIcons = ['SiPostgresql','SiMongodb'];
   const frontIcons = ['RiNextjsFill', 'LiaReact', 'SiTailwindcss', 'SiShadcnui'];
   const stateIcons = ['RiNextjsFill', 'LiaReact', 'SiTailwindcss'];
+  const authIcons = ['RiFirebaseFill', 'RiSupabaseLine', 'SiJsonwebtokens', 'TbBrandNextjs'];
+  const apiIcons = ['TbApiOff', 'GrGraphQl'];
   const toolsIcons = ['FaGitAlt', 'FaGithub', 'SiPostman', 'RiVercelFill', 'SiNetlify'];
 
   // Icons Map
@@ -33,8 +53,14 @@ export default function Skills() {
   }
   const backendIconMap = {
     SiNodedotjs : SiNodedotjs,
-    SiMongodb : SiMongodb,
+    SiExpress: SiExpress,
+    SiNestjs: SiNestjs,
+    SiPrisma: SiPrisma,
     SiMongoose : SiMongoose
+  }
+  const databaseIconMap = {
+    SiPostgresql : SiPostgresql,
+    SiMongodb: SiMongodb,
   }
   const frontendIconMap = {
     RiNextjsFill : RiNextjsFill,
@@ -46,6 +72,16 @@ export default function Skills() {
     RiNextjsFill : RiNextjsFill,
     LiaReact : LiaReact,
     SiTailwindcss : SiTailwindcss,
+  }
+  const authIconMap = {
+    RiFirebaseFill : RiFirebaseFill,
+    RiSupabaseLine : RiSupabaseLine,
+    SiJsonwebtokens : SiJsonwebtokens,
+    TbBrandNextjs : TbBrandNextjs
+  }
+  const apiIconMap = {
+    TbApiOff : TbApiOff,
+    GrGraphQl : GrGraphQl
   }
   const toolsIconMap = {
     FaGitAlt : FaGitAlt,
@@ -99,6 +135,31 @@ export default function Skills() {
         </div>
       </section>
 
+
+      
+      {/* Database  */}
+      <section className="mt-10">
+        {/* Title  */}
+        <DetailsSubTitle title={"Database"}/>
+
+        <div className="mt-5 flex flex-wrap gap-2">
+        {
+            databaseIcons.map((ele,i)=>{
+              const GetIcon = databaseIconMap[ele];
+              console.log(GetIcon)
+              return (
+                <div key={i} className="bordr-[0.1px] p-1 px-3 bg-[#1E293B] flex gap-4" style={{background: "linear-gradient(to bottom right, hsl(0, 0%, 25%) 0%, hsla(0, 0%, 25%, 0) 50%)"}}>
+                  <div className="mt-1 text-xl text-primary">
+                    {GetIcon ? <GetIcon /> : null}
+                  </div>
+                  <p className="font-semibold">{database[i]}</p>
+                </div>
+              )
+            })
+        }
+        </div>
+      </section>
+
       {/* FrontEnd Techonologys  */}
       <section className="mt-10">
         {/* Title  */}
@@ -145,6 +206,56 @@ export default function Skills() {
         }
         </div>
       </section>
+
+
+      
+      {/* Authentication & Authorization */}
+      <section className="mt-10">
+        {/* Title  */}
+        <DetailsSubTitle title={"Authentication & Authorization"}/>
+
+        <div className="mt-5 flex flex-wrap gap-2">
+        {
+            authIcons.map((ele,i)=>{
+              const GetIcon = authIconMap[ele];
+              console.log(GetIcon)
+              return (
+                <div key={i} className="bordr-[0.1px] p-1 px-3 bg-[#1E293B] flex gap-4" style={{background: "linear-gradient(to bottom right, hsl(0, 0%, 25%) 0%, hsla(0, 0%, 25%, 0) 50%)"}}>
+                  <div className="mt-1 text-xl text-primary">
+                    {GetIcon ? <GetIcon /> : null}
+                  </div>
+                  <p className="font-semibold">{auth[i]}</p>
+                </div>
+              )
+            })
+        }
+        </div>
+      </section>
+
+
+      {/* API Management */}
+      <section className="mt-10">
+        {/* Title  */}
+        <DetailsSubTitle title={"API Development"}/>
+
+        <div className="mt-5 flex flex-wrap gap-2">
+        {
+            apiIcons.map((ele,i)=>{
+              const GetIcon = apiIconMap[ele];
+              console.log(GetIcon)
+              return (
+                <div key={i} className="bordr-[0.1px] p-1 px-3 bg-[#1E293B] flex gap-4" style={{background: "linear-gradient(to bottom right, hsl(0, 0%, 25%) 0%, hsla(0, 0%, 25%, 0) 50%)"}}>
+                  <div className="mt-1 text-xl text-primary">
+                    {GetIcon ? <GetIcon /> : null}
+                  </div>
+                  <p className="font-semibold">{api[i]}</p>
+                </div>
+              )
+            })
+        }
+        </div>
+      </section>
+
 
 
        {/* Tools */}
